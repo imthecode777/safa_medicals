025d1c98fa7aa86b94f961d50dbbe75bbe221202
Fahad Siraj <fahadsiraj202281@gmail.com>
2025-08-12 23:37:48 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,15 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/safa_medicals" : "/",
+  base: "/safa_medicals",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === "development" && componentTagger(),
+    mode === "development" &&
+      componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
